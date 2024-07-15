e602e5c023297e9b96f69d1de5f97715aeb93d2d
omar-sarfraz <omar.sarfraz@arbisoft.com>
2024-07-15 12:20:04 +0500

    [Add] Basic Demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+const cipher = require("@omar-sarfraz/caesar-cipher");
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [key, setKey] = useState<number>(1);
+    const [message, setMessage] = useState<string>("");
+    const [result, setResult] = useState<string>("");
+
+    const encryptMessage = (): void => {
+        const { encrypt } = cipher(key);
+        let encryptedMessage = encrypt(message);
+        setResult(encryptedMessage);
+    };
+
+    const decryptMessage = (): void => {
+        const { decrypt } = cipher(key);
+        let decryptedMessage = decrypt(message);
+        setResult(decryptedMessage);
+    };
+
+    return (
+        <div>
+            <div>
+                <label>Key</label>
+                <input
+                    type="number"
+                    placeholder="Enter numeric key"
+                    value={key}
+                    onChange={(e) => setKey(parseInt(e.target.value))}
+                />
+            </div>
+            <div>
+                <label>Message</label>
+                <input
+                    type="text"
+                    placeholder="Type your email, phonenumber or a message"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                />
+            </div>
+            <div>
+                <button onClick={encryptMessage}>Encrypt</button>
+                <button onClick={decryptMessage}>Decrypt</button>
+            </div>
+            {result && (
+                <div>
+                    <h3>Result</h3>
+                    <p>{result}</p>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
