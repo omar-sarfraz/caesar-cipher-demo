89ea342226d3d2c1dde413c3082297e1fc30d5b0
omar-sarfraz <omar.sarfraz@arbisoft.com>
2024-07-15 13:02:05 +0500

    [Add] Basic Designing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./App.css";
 const cipher = require("@omar-sarfraz/caesar-cipher");
 
 function App() {
@@ -19,35 +20,38 @@ function App() {
     };
 
     return (
-        <div>
-            <div>
-                <label>Key</label>
-                <input
-                    type="number"
-                    placeholder="Enter numeric key"
-                    value={key}
-                    onChange={(e) => setKey(parseInt(e.target.value))}
-                />
-            </div>
-            <div>
-                <label>Message</label>
-                <input
-                    type="text"
-                    placeholder="Type your email, phonenumber or a message"
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                />
-            </div>
-            <div>
-                <button onClick={encryptMessage}>Encrypt</button>
-                <button onClick={decryptMessage}>Decrypt</button>
-            </div>
-            {result && (
-                <div>
-                    <h3>Result</h3>
-                    <p>{result}</p>
+        <div className="main">
+            <div className="card">
+                <h1>Caesar Cipher Demo</h1>
+                <div className="input">
+                    <label>Key</label>
+                    <input
+                        type="number"
+                        placeholder="Enter numeric key"
+                        value={key}
+                        onChange={(e) => setKey(parseInt(e.target.value))}
+                    />
                 </div>
-            )}
+                <div className="input">
+                    <label>Message</label>
+                    <input
+                        type="text"
+                        placeholder="Type an email or a phonenumber"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                    />
+                </div>
+                <div className="buttons">
+                    <button onClick={encryptMessage}>Encrypt</button>
+                    <button onClick={decryptMessage}>Decrypt</button>
+                </div>
+                {result && (
+                    <div>
+                        <h3>Result</h3>
+                        <p>{result}</p>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
